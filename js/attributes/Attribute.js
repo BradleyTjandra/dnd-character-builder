33c6ac02e021a01c9f1b625236d1c2eaf63887a4
BradleyTjandra <bjtjandra@gmail.com>
2021-10-05 09:36:28 +1100

    Change concat of Sets to be done correctly

--- js/attributes/Attribute.js
+++ js/attributes/Attribute.js
@@ -30,7 +30,7 @@ export default class Attribute {
 
     // if fixed we don't calculate this, this is set by the user
     if (this.calcType == "calculated") {
-      console.log(`calculating ${this.name}`);
+      // console.log(`calculating ${this.name}`);
       this.value = this.inputs.reduce((sum, i) => sum+parseFloat(i.value?.formula ?? 0), 0);
     } else if (this.calcType == "concat") {
       this.value = this.inputs.map(item => item.value);
--- js/links/Link.js
+++ js/links/Link.js
@@ -26,20 +26,10 @@ export class Link {
     this.type = type; 
     this.attribute = attribute;
     this.info = info;
-    // if (this.attribute.name == "con") console.log("attributes list!");
-    // if (this.attribute.name == "con") console.log(this._attributes);
   }
 
   refreshinfoValidity() {
-
     this._info["valid"] = true;
-
-    // if (this.type.includes("calculated")) {
-    //   this._info["valid"] = this.info.getValidity();
-    // } else {
-    //   this._info["valid"] = true;
-    // }
-
   }
 
   get value() {
@@ -47,8 +37,6 @@ export class Link {
     if (!this.isSetup) return undefined;
 
     if (!this._formula) return this.info;
-    // if (this.attribute.name == "con") console.log("get the formula");
-    // if (this.attribute.name == "con") console.log(this._formula);
 
     let formulaToValues = (calc) => {
       if (calc.constructor.name == "Calculation") return (calc.calculate());
@@ -60,8 +48,6 @@ export class Link {
       if (formulae.constructor.name == "String") evaluated[key] = formulae;
       else evaluated[key] = formulae.map(formulaToValues).join("");
     }
-    // if (this.attribute.name == "con") console.log("get evaluated");
-    // if (this.attribute.name == "con") console.log(evaluated);
 
     return(evaluated);
   }
@@ -72,59 +58,21 @@ export class Link {
 
     // update effect info
     let oldInfo = Object.assign({}, this._info['data']);
-    // if (this.attribute.name == "con") {
-    //   console.log("oldinfo");
-    //   console.log(oldInfo);
-    //   console.log("newinfo");
-    //   console.log(newInfo);
-    // }
     if (oldInfo == newInfo) return;
     this._info['data'] = newInfo;
     this._formula = {};
 
-    // let tmp = Object.assign(newInfo);
-    // delete tmp.formula;
-    // if (!isEmptyObj(tmp)) {
-    //   console.log(this.attribute.name);
-    //   console.log(tmp);
-    // }
-    // if (this.attribute.name == "con") {
-    //   console.log("oldinfo");
-    //   console.log(oldInfo);
-    //   console.log("newinfo");
-    //   console.log(newInfo);
-    // }
-    // console.log(newInfo);
-
     for (let [key, value] of Object.entries(newInfo)) {
 
-    // if ("formula" in newInfo) {
-        // if (this.attribute.name == "con") console.log("con text");
-        // if (this.attribute.name == "con") console.log(value);
-        // if (this.attribute.name == "con") console.log(this._attributes);
-        let calc = this.calculationFromFormulaText(value);
-        // if (this.attribute.name == "con") console.log(calc);
-        this._formula[key] = calc; 
-
-        // this._info["valid"] = calc.reduce( (prev, c) => {
-        //   if (typeof c === "string") return prev;
-        //   return (prev && c.getValidity());
-        // }, true);
-
-        this._info["valid"] = true;
-
-        this.inputs = Array.from(
-          calc.reduce(getAllInputs, new Set())
-        );
-        
-      // } else {
-      //   this._info["valid"] = true;
-      //   this.inputs = [];
-      // }
+      let calc = this.calculationFromFormulaText(value);
+      this._formula[key] = calc; 
+      this._info["valid"] = true;
+      this.inputs = Array.from(
+        calc.reduce(getAllInputs, new Set())
+      );
     }
 
     this.isSetup = (this._info["valid"] && this._attribute["valid"]);
-    if (this.attribute.name == "con") console.log("setup is: " + this.isSetup);
 
     if (this.isSetup) {
       this.inputs.forEach((attr) => attr.listeners.push(this));
@@ -201,38 +149,28 @@ export class Link {
 
   calculationFromFormulaText(text) {
 
-    // if (text == "{{base-ability-score-con}}") console.log("here I go adding things");
-
     let splitTexts;
     if (/[^\\]\$/.test(text)) {
       splitTexts = text.split(/(?<!\\)\$/);
     } else {
       splitTexts = ["", text];
     }
-    // if (text == "{{base-ability-score-con}}") console.log(splitTexts);
 
     let calcs = splitTexts.map( (text, i) => {
       if (i % 2 == 0) return text;
       if (text[0] == "+" || text[0] == "-" || /^\s*$/.test(text))  {
         text = "0" + text;
       } 
-      // let calc = new Calculation(formulaText, this._attributes, this);
       let calc =  new Calculation(text, this._attributes, this);
-
-      // console.log(calc);
-      // console.log(getValidity());
       if (calc.getValidity()) return calc;
       return text;
-      // else return new Calculation(txt, this._attributes, this);
     });
     if (!calcs) return([]);
-    // if (text == "{{base-ability-score-con}}") console.log("I am just about to return");
-    // if (text == "{{base-ability-score-con}}") console.log(calcs);
     return(calcs);
   }
 }
 
 function getAllInputs(prev, calc) {
-  if (calc.constructor.name == "Calculation") return new Set(prev, calc.getInputs());
+  if (calc.constructor.name == "Calculation") return new Set([...prev, ...calc.getInputs()]);
   else return prev;
 }
